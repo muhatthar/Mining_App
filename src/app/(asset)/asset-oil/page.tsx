c4ed5f6fde27e73bd4fc246abb86655dc492efae
MuhAtthar <muhatthar@student.ub.ac.id>
2023-12-04 00:14:50 +0700

    modal

--- src/app/(asset)/asset-oil/page.tsx
+++ src/app/(asset)/asset-oil/page.tsx
@@ -1,80 +1,85 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+import Modal from "@/app/components/Modal";
 
 export default function AssetOil() {
   return (
     <>
-      <div className="flex flex-col px-[40px]">
-        <div className="flex w-full justify-end">
-          <button className="w-[140px] flex items-center gap-[9.25px] justify-center mb-4 rounded-xl py-2 border-2 border-transparent transition-all duration-200 ease-in-out hover:border-[#F99417]">
-            <p className="text-[#545F71] font-medium text-xl">Add List</p>
-            <PlusCircleIcon className="w-[31.5px] h-[31.5px] text-[#F99417]"></PlusCircleIcon>
-          </button>
+      <Fragment>
+        <div className="flex flex-col px-[40px]">
+          <div className="flex w-full justify-end">
+            <button className="w-[140px] flex items-center gap-[9.25px] justify-center mb-4 rounded-xl py-2 border-2 border-transparent transition-all duration-200 ease-in-out hover:border-[#F99417]">
+              <p className="text-[#545F71] font-medium text-xl">Add List</p>
+              <PlusCircleIcon className="w-[31.5px] h-[31.5px] text-[#F99417]"></PlusCircleIcon>
+            </button>
+          </div>
+          <div className="w-full text-center table-auto border border-[#DDE1E6] rounded-lg">
+            <table className="w-full">
+              <thead className="bg-[#F2F4F8] h-12 border-b border-[#DDE1E6]">
+                <tr>
+                  <th className="rounded-tl-lg">ID</th>
+                  <th>Nama Aset</th>
+                  <th>Jenis Aset</th>
+                  <th>Status Aset</th>
+                  <th className="rounded-tr-lg">Riwayat Status</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>1</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>2</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>3</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>4</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>5</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12 border-b border-[#DDE1E6]">
+                  <td>6</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td>Tidak ada</td>
+                </tr>
+                <tr className="h-12">
+                  <td className="rounded-bl-lg">7</td>
+                  <td>Minyak Goreng</td>
+                  <td>Minyak</td>
+                  <td>Basi</td>
+                  <td className="rounded-br-lg">Tidak ada</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="w-full text-center table-auto border border-[#DDE1E6] rounded-lg">
-          <table className="w-full">
-            <thead className="bg-[#F2F4F8] h-12 border-b border-[#DDE1E6]">
-              <tr>
-                <th className="rounded-tl-lg">ID</th>
-                <th>Nama Aset</th>
-                <th>Jenis Aset</th>
-                <th>Status Aset</th>
-                <th className="rounded-tr-lg">Riwayat Status</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white">
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>1</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>2</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>3</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>4</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>5</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12 border-b border-[#DDE1E6]">
-                <td>6</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td>Tidak ada</td>
-              </tr>
-              <tr className="h-12">
-                <td className="rounded-bl-lg">7</td>
-                <td>Minyak Goreng</td>
-                <td>Minyak</td>
-                <td>Basi</td>
-                <td className="rounded-br-lg">Tidak ada</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+        <Modal></Modal>
+      </Fragment>
     </>
   );
 }
---
+++ src/app/components/Modal.tsx
@@ -0,0 +1,7 @@
+export default function Modal() {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <h1>Modal</h1>
+    </div>
+  );
+}
